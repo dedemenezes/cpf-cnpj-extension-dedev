92ed944fe578dfe7559bec34be36428988a1b1f1
Andre Menezes <djdedemenezes@gmail.com>
2026-01-22 15:10:04 -0300

    add keybaord shortcuts

--- dist/background.js
+++ dist/background.js
@@ -1,3 +1,16 @@
+// Listener para os comandos de teclado
+chrome.commands.onCommand.addListener((command) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      if (command === "generate-cpf") {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "gerarCPF" });
+      } else if (command === "generate-cnpj") {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "gerarCNPJ" });
+      }
+    }
+  });
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "gerarCPF",
